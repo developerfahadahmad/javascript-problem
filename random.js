3fbcecfcfef801bd6b75e25eb36e9c233828a7bd
developerfahadahmad <mdshahariarhafiz2022@gmail.com>
2022-06-24 21:25:11 +0600

    added

---
+++ random.js
@@ -0,0 +1,22 @@
+
+let num = 2.452434556;
+let figure = Math.floor(num);
+console.log(figure);
+
+
+
+let num2 = 4.678953224;
+let figure2 = Math.ceil(num2);
+console.log(figure2);
+
+
+
+let num3 = 5.5670890;
+let figure3 = Math.round(num3);
+console.log(figure3);
+
+
+
+let randomNumber = Math.random()*6;
+let figure4 = Math.round(randomNumber);
+console.log(figure4);
